eb704156e44d9462415f607cea574eaf753a7da1
Aiswarya Patra <147045040+tomoe21@users.noreply.github.com>
2024-10-29 00:30:13 +0530

    Add files via upload

---
+++ c program/q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int item;
+    int product;
+    if(item!= 0)
+    {
+        product= product*item;
+        printf("Product: %d\n", product);
+    }
+    else
+    {
+        printf("Product: %d\n", product);
+    }
+    return 0;
+}
